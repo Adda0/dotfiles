f5e8c4597028370f87721a13ba4de91f4b030127
David Chocholatý <chocholaty.david@protonmail.com>
2020-08-30 10:47:00 +0200

    Move some configs from .zshrc to shortcuts.zsh file

---
+++ .config/zsh/.oh-my-zsh/custom/shortcuts.zsh
@@ -0,0 +1,68 @@
+# Use vim keys in tab complete menu:
+bindkey -M menuselect '^h' vi-backward-char
+bindkey -M menuselect '^k' vi-up-line-or-history
+bindkey -M menuselect '^l' vi-forward-char
+bindkey -M menuselect '^j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+
+# Edit line in vim with ctrl-e:
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^e' edit-command-line
+
+# Custom ZSH Binds
+bindkey '^ ' autosuggest-accept
+
+# Use lf to switch directories and bind it to ctrl-o
+function lfcd {
+    tmp="$(mktemp)"
+    lf -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+    fi
+}
+#bindkey -s '^o' 'lfcd\n'
+bindkey '\eq' push-input
+bindkey -s '^o' '\eq lfcd\n'
+
+rangercd () {
+    tmp="$(mktemp)"
+    ranger --choosedir="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        [ --datadir "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+        #[ -d "$dir" ] TODO which one?
+    fi
+}
+bindkey -s '^g' 'rangercd\n'
+
+	# ci"
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+  for c in {a,i}{\',\",\`}; do
+    bindkey -M $m $c select-quoted
+  done
+done
+
+# ci{, ci(, di{ etc..
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+  for c in {a,i}${(s..)^:- '()[]{}<>bB'}; do
+    bindkey -M $m $c select-bracketed
+  done
+done
+
+
+function ctrlz {
+  if [[ $#BUFFER == 0 ]]; then
+    fg >/dev/null 2>&1 && zle redisplay
+  else
+    zle push-input
+  fi
+}
+zle -N ctrlz
+bindkey '^Z' ctrlz
