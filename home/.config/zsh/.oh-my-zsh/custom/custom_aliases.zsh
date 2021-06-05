1064309f8d9ad4b9337fd0f275087fb6b89ae10c
David Chocholatý <chocholaty.david@protonmail.com>
2021-06-05 16:22:41 +0200

    TMP: Stop using 'trash' for the time being

--- home/.config/zsh/.oh-my-zsh/custom/custom_aliases.zsh
+++ home/.config/zsh/.oh-my-zsh/custom/custom_aliases.zsh
@@ -286,7 +286,7 @@ alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
 
-alias rm='trash'
+#alias rm='trash'
 # adding flags
 alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
